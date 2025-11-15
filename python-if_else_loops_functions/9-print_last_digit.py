5013e789b181b2598779bc64d2734531f3d603f3
theshasec <theshasec@gmail.com>
2025-11-15 06:34:03 +0000

    343

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
-def print_last_digit(number):
+def print_last_digit(number):i
+    try:
+        a=int(number)
+    except ValueError:
+        print("Traceback (most recent call last):")
+        return
     print(str(number)[-1],end="")
     return str(number)[-1]
