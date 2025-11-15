6360c7ebfa2019b470e0b047c7a89473ae0d3411
theshasec <theshasec@gmail.com>
2025-11-15 06:38:56 +0000

    43

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     try:
-        a=int(number)
+        a = int(number)
     except ValueError:
         print("Traceback (most recent call last):")
         return
-    print(str(number)[-1],end="")
+    print(str(number)[-1], end="")
     return str(number)[-1]
