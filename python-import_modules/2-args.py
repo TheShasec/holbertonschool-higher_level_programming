cd159780df9312ad4739813475b6ec4a1fc4baca
theshasec <theshasec@gmail.com>
2025-11-17 05:42:20 +0000

    3243

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    args = sys.argv
+    l = len(args)
+    if l-1 == 0:
+        print("{} arguments.".format(l-1))
+    else:
+        print("{} arguments:".format(l-1))
+        for i in range(1,l):
+            print("{}: {}".format(i,args[i]))
