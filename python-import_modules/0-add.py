76d0dc0eb65ff4ff6ec03cbfda2339101e612a7c
theshasec <theshasec@gmail.com>
2025-11-17 05:18:47 +0000

    342

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add
-a = 1
-b = 2
-r = add(1, 2)
 if __name__ ==  "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    r = add(1, 2)
     print("{} + {} = {}".format(a, b, r))
