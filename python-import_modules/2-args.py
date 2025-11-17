6aa2ea5e88938470e5df721310a54dfe20f4b54b
theshasec <theshasec@gmail.com>
2025-11-17 05:47:46 +0000

    4234

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,13 +2,13 @@
 if __name__ == "__main__":
     import sys
     args = sys.argv
-    l = len(args)
-    if l-1 == 0:
-        print("{} arguments.".format(l-1))
+    myl = len(args)
+    if myl-1 == 0:
+        print("{} arguments.".format(myl-1))
     else:
-        if l-1 == 1:
-            print("{} argument:".format(l-1))
+        if myl-1 == 1:i
+            print("{} argument:".format(myl-1))
         else:
-            print("{} arguments:".format(l-1))
-        for i in range(1,l):
-            print("{}: {}".format(i,args[i]))
+            print("{} arguments:".format(myl-1))
+        for i in range(1, myl):
+            print("{}: {}".format(i, args[i]))
