b3a79b35cfa7068de5119c63884a6ab5200e5d6a
theshasec <theshasec@gmail.com>
2025-11-18 10:13:48 +0000

    4234

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,15 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    al = len(tuple_a)
-    bl = len(tuple_b)
-    ll = 0
-    new = []
-    if al > bl:
-        for i in range(al):
-            if i < bl:
-                new.append(tuple_a[i]+tuple_b[i])
-    else:
-        for i in range(bl):
-            if i < al:
-                new.append(tuple_a[i]+tuple_b[i])
+    for i in range(0,2):
+        new.append(tuple_a[i]+tuple_b[i])
     return tuple(new)
