69a90a98b77a57ecb4d4958b105bb19826dd720a
theshasec <theshasec@gmail.com>
2025-11-18 10:48:31 +0000

    3423

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -2,9 +2,4 @@
 def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return my_list
-    a = []
-    for i in range(len(my_list)):
-        if i != idx:
-            a.append(my_list[i])
-    my_list = a
-    return a
+    return del my_list[idx]
