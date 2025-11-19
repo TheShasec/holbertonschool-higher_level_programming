aeedf933573140dc671dbbba9a726c1fe4bfadc4
theshasec <theshasec@gmail.com>
2025-11-19 19:15:33 +0000

    4324

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(0,x):
             print(my_list[i], end="")
             t = t + 1
-    except:
+    except Exception:
         pass
-    finally:
-        return t
+    print()
+    return t
