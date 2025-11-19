0adf5f84c6e35f7573fad1a6f20fb6bcd659ecf7
theshasec <theshasec@gmail.com>
2025-11-19 13:48:40 +0000

    3234

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if len(a_dictionary.keys()) == 0:
+    if len(a_dictionary.keys()) == 0 or a_dictionary == None:
         return None
     tl = max(a_dictionary.values())
     for i in a_dictionary:
