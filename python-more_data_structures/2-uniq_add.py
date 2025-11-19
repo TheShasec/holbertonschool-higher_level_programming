95337ac110ffe05b82af8e61f018aec2a11bd511
theshasec <theshasec@gmail.com>
2025-11-19 11:20:11 +0000

    4234

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    at = []
+    ss = 0
+    for i in my_list:
+        if i not in at:
+            at.append(i)
+            ss = ss + i
+    return ss
+
+
