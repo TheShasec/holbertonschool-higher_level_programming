6531139c8ce9f2ddc46391375fe509aa44cd74b9
theshasec <theshasec@gmail.com>
2025-11-19 13:06:51 +0000

    343

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    tl = list(a_dictionary.keys())
+    tl.sort()
+    for i in tl:
+        print("{}: {}".format(i,a_dictionary[i]))
