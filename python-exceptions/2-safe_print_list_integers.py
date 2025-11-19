733bc22e99b1f446325fecf4fe31caaebdbdc383
theshasec <theshasec@gmail.com>
2025-11-19 19:52:15 +0000

    34234

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    t = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]))
+        except:
+            pass
+        t = t + 1
+    return t
