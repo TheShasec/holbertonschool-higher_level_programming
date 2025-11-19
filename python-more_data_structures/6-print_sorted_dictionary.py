1f442781e5316a84e3be30eb9c52f900df97cb33
theshasec <theshasec@gmail.com>
2025-11-19 13:07:38 +0000

    434

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,4 +3,4 @@ def print_sorted_dictionary(a_dictionary):
     tl = list(a_dictionary.keys())
     tl.sort()
     for i in tl:
-        print("{}: {}".format(i,a_dictionary[i]))
+        print("{}: {}".format(i, a_dictionary[i]))
