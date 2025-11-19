26530ad482c0e9a8d1ec5988db47771d2239422c
theshasec <theshasec@gmail.com>
2025-11-19 11:13:24 +0000

    fdkfdf

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new  = my_list[::]
+    new = my_list[::]
     for i in range(len(my_list)):
         if new[i] == search:
             new[i] = replace
