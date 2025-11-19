b57cd242edbbf4f2a0c51f1018bff6b329388aa7
theshasec <theshasec@gmail.com>
2025-11-19 19:53:49 +0000

    3423

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,7 @@ def safe_print_list_integers(my_list=[], x=0):
     t = 0
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i]))
+            print("{:d}".format(my_list[i]), end="")
         except:
             pass
         t = t + 1
