d92b5134fe92d3479f9adf9fe8136bfe4d707a29
theshasec <theshasec@gmail.com>
2025-11-20 12:24:30 +0000

    234

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    r = 0
+    r = None
     try:
         r = a / b
     except Exception:
         return None
     finally:
+        print("Inside result: {}".format(r))
         return r
