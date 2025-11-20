8ae258397df99b34b4e721097d99bc29081621e2
theshasec <theshasec@gmail.com>
2025-11-20 12:44:37 +0000

    32434

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    ml = []
+    try:
+        for i in range(list_length):
+            ml.append(my_list_1[i] / my_list_2[i])
+    except ZeroDivisionError:
+        print("division by 0")
+    except TypeError:
+        print("wrong type")
+    except IndexError:
+        print("out of range")
+    finally:
+        return ml
