14797e04abdb35fffbd3ee3022c4aa05a78f5b5a
theshasec <theshasec@gmail.com>
2025-11-21 11:16:12 +0000

    234

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -7,7 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
             t = t + 1
         except IndexError as i:
             raise i
-        except:
+        except Exception:
             pass
     print()
     return t
