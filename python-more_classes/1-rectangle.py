2c3155a5a2fb29741cf466b90813987443ec6761
theshasec <theshasec@gmail.com>
2025-11-24 17:31:34 +0000

    34324

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"dsafdfdsf"
+
+
+class Rectangle:
+    "dafsdf"
+
+    @property
+    def width(self):
+        return self.__width
+    
+    @width.setter
+    def width(self, vl):
+        if not isinstance(vl, int):
+            raise TypeError("width must be an integer")
+        if vl < 0:
+            raise ValueError("ValueError")
+        self.__width = vl
+    
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, vl):
+        if not isinstance(vl, int):
+            raise TypeError("height must be an integer")
+        if vl < 0:
+            raise ValueError("ValueError")
+        self.__height = vl
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+    
