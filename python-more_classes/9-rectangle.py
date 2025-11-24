9008f9415c99f9e0dec0e9989f3f30dfcdda2e55
theshasec <theshasec@gmail.com>
2025-11-24 18:27:21 +0000

    4234

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -69,7 +69,7 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
-        return cls(size,size)
+        return cls(size, size)
 
     def __init__(self, width=0, height=0):
         self.width = width
