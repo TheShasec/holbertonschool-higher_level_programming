e380e649da92dec32ed043d62258ce57c926a134
theshasec <theshasec@gmail.com>
2025-11-24 17:56:26 +0000

    4234

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -38,6 +38,8 @@ class Rectangle:
         return 2 * (self.width + self.height)
 
     def __str__(self):
+        if self.width == 0 or self.height == 0:
+            return ""
         at = ""
         for i in range(self.height):
             at += "#" * self.width
