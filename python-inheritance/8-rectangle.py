456513db1335658504d4e6171a8269aec0cd6eb0
theshasec <theshasec@gmail.com>
2025-11-25 20:28:31 +0000

    3242

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 """3323"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-class Rectangle:
+class Rectangle(BaseGeometry):
     """43434"""
 
     def __init__(self, width, height):
-        if type(width) is not int:
-            raise TypeError("width must be an integer")
-        if type(height) is not int:
-            raise TypeError("height must be an integer")
-        if not hasattr(self, 'width'):
-            raise AttributeError("'Rectangle' object has no attribute 'width'")
-        if not hasattr(self, 'height'):
-            raise AttributeError("'Rectangle' object has no attribute 'height'")
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
