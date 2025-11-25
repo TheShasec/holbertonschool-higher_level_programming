fb8bc551b2f50dd0f1dd03d8c9f6448b3ebdcbe8
theshasec <theshasec@gmail.com>
2025-11-25 13:27:04 +0000

    435

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -4,12 +4,6 @@
 class MyList(list):
     """3323"""
 
-    def append(self, value):
-        super().append(value)
-
-    def __str__(self):
-        return super().__str__()
-
     def print_sorted(self):
         al = self[::]
         al.sort()
