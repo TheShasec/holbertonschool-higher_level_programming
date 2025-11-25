37d0c77e8e2a6896d35939ceec3c71a2ce83d324
theshasec <theshasec@gmail.com>
2025-11-25 20:37:49 +0000

    3242

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -7,7 +7,7 @@ class Square(Rectangle):
     """32323"""
 
     def __init__(self, size):
-        self.integer_validator("size",size)
+        self.integer_validator("size", size)
         self.__size = size
 
     def area(self):
