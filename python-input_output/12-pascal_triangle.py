bc5534054d42999e2b9314b720f8b948d788340c
theshasec <theshasec@gmail.com>
2025-11-26 18:54:45 +0000

    4354

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -10,11 +10,14 @@ def fl(a):
 
 def pascal_triangle(n):
     """543"""
-
+    if n <= 0:
+        return []
+    az = []
     it = 0
     while it < n :
         a = []
         for i in range(0, it+1):
             a.append(fl(it)//(fl(i)*fl(it-i)))
-        print(a)
+        az.append(a)
         it += 1
+    return az
