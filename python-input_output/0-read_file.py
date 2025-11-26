5714380f6b914c29944654812ecf4215ec70642a
theshasec <theshasec@gmail.com>
2025-11-26 12:49:28 +0000

    324

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,5 +5,5 @@
 def read_file(filename=""):
     """32443"""
 
-    with open(filename,encoding="utf-8") as mf:
-        print(mf.read().rstrip())
+    with open(filename, encoding="utf-8") as mf:
+        print(mf.read(), end="")
