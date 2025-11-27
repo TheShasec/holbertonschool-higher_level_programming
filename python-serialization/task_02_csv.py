655698ce2da483d69166e03d5bec313d8c7428dd
theshasec <theshasec@gmail.com>
2025-11-27 10:28:46 +0000

    3545

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -9,9 +9,9 @@ def convert_csv_to_json(filename):
     """ 43 """
     try:
         with open(filename, "r") as mf:
-            data = csv.DictReader(mf)
+            data = list(csv.DictReader(mf))
         with open("data.json", "w") as mf:
-            json.dump(list(data), mf)
+            json.dump(data, mf)
         return True
     except Exception:
         return False
