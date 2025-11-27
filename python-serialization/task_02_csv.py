01319cc1781030bddbecc27f34bc95388e7859a0
theshasec <theshasec@gmail.com>
2025-11-27 10:21:04 +0000

    3545

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" 434 """
+
+
+import csv
+import json
+
+def convert_csv_to_json(filename):
+    """ 43 """
+    try:
+        with open(filename, "r") as mf:
+            reader = csv.DictReader(mf)
+            json.dump(reader, "data.json")
+        return True
+    except Exception:
+        return False
