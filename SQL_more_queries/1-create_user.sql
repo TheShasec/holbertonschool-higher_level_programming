b38625ecf39d5656dd32869ae140f725b590b93c
theshasec <theshasec@gmail.com>
2025-11-28 13:01:43 +0000

    43

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- 545
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd' IF NOT EXISTS ;
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO "user_0d_1"@"localhost";
