3b9fb80d07e65ec429541d1cd9a7752948d02084
theshasec <theshase@gmail.com>
2025-11-29 08:17:31 +0000

    34234

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,2 +1,2 @@
 -- j
-SELECT tv_shows.title , tv_show_genres.genre_id FROM tv_show_genres RIGHT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id WHERE tv_show_genres.genre_id=NULL ORDER BY tv_shows.title ASC , tv_show_genres.genre_id ASC
+SELECT tv_shows.title , tv_show_genres.genre_id FROM tv_show_genres RIGHT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id WHERE tv_show_genres.genre_id IS NULL ORDER BY tv_shows.title ASC , tv_show_genres.genre_id ASC
