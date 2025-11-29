102ba8ff082426752e13c81eba81cbaf0a8ce34d
theshasec <theshase@gmail.com>
2025-11-29 07:57:32 +0000

    34234

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- ggh
-SELECT cities.id , cities.name , states.name FROM cities LEFT JOIN ON cities.state_id = states.id 
+SELECT cities.id , cities.name , states.name FROM cities LEFT JOIN states ON cities.state_id = states.id 
