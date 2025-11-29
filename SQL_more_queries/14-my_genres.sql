ee7bde0481b2c0993b52c55def2c1dd91cdf1e62
theshasec <theshase@gmail.com>
2025-11-29 08:40:17 +0000

    34234

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,2 +1,2 @@
 -- djf
-SELECT tv_genres.name FROM tv_show_genres JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id WHERE tv_shows.name = Dexter;
+SELECT tv_genres.title FROM tv_show_genres JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id WHERE tv_shows.name = "Dexter";
