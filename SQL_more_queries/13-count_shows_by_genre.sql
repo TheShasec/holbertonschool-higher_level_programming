4f0b40a49e483c503a444f8a554b3fc33b5d6e37
theshasec <theshase@gmail.com>
2025-11-29 08:31:47 +0000

    34234

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,2 +1,2 @@
 -- fdssd
-SELECT tv_genres.name , COUNT(*) AS number_of_shows FROM tv_show_genres JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id GROUP BY tv_show_genres.genre_id;
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows FROM tv_show_genres JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id GROUP BY tv_show_genres.genre_id;
