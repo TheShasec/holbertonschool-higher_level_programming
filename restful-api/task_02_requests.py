0b669b54f7481407561f065104b6038c923eb3c0
theshasec <theshasec@gmail.com>
2025-12-01 13:16:56 +0000

    jkdf

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -21,11 +21,11 @@ def fetch_and_save_posts():
         r = req.get(url)
         posts = r.json()
         keys = ["id", "title", "body"]
-
+        filtered_posts = [{field: post[field] for field in selected_fields} for post in posts]
         with open("posts.csv", "w", newline="", encoding="utf-8") as mf:
             writer = csv.DictWriter(mf, fieldnames=keys)
             writer.writeheader()
-            writer.writerows(posts)
+            writer.writerows(filtered_posts)
 
     except Exception:
         raise Exception
