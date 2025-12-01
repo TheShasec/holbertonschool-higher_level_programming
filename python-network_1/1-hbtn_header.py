48b1597ba7863f21b3eaa4837a5467a6480b71ad
theshasec <theshasec@gmail.com>
2025-12-01 04:54:28 +0000

    4324

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -6,7 +6,6 @@ import sys
 import urllib.request
 
 if __name__ == "__main__":
-    print(sys.argv)
     url = sys.argv[1]
     with urllib.request.urlopen(url) as response:
         print(response.getheader("X-Request-Id"))
