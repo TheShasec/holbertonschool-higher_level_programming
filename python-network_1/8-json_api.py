6e74ec234e766863e6f10019fdc0b55d4b6254b4
theshasec <theshasec@gmail.com>
2025-12-01 05:59:24 +0000

    4324

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -9,9 +9,9 @@ import sys
 if __name__ == "__main__":
     q = ""
     if len(sys.argv) == 2:
-        q=sys.argv[1]
+        q = sys.argv[1]
     url = "http://0.0.0.0:5000/search_user"
-    r = req.post(url, data = {"q": q})
+    r = req.post(url, data={"q":q})
     try:
         if len(r.json()) == 0:
             print("No result")
