5910e6dc2e110d41ab27e49d5a1e0b3d43b8796b
theshasec <theshasec@gmail.com>
2025-12-01 05:20:37 +0000

    4324

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -8,5 +8,5 @@ import requests as req
 if __name__ == "__main__":
     myreq = req.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print("\n - type: {}".format(type(myreq.text)))
-    print("\n - content: {}".format(myreq.text))
+    print("\t - type: {}".format(type(myreq.text)))
+    print("\t - content: {}".format(myreq.text))
